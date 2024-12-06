a4b11f9c53324d38a7d4858ab84f1d2f0443f7a9
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-12-07 04:33:27 +0530

    laoding function added

--- lib/Services/loadmenuitems.dart
+++ lib/Services/loadmenuitems.dart
@@ -1,17 +1,27 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 
+import '../models/ItemModel.dart';
 import '../models/categorymodel.dart';
 import '../models/menu-item.dart';
 
+//menu items
 Future<MenuResponse> loadMenuItems() async {
   final String response = await rootBundle.loadString('assets/dataset.json');
   final Map<String, dynamic> data = json.decode(response);
   return MenuResponse.fromJson(data);
 }
 
+//categories
 Future<CategoryResponse> loadCategories() async {
   final String response = await rootBundle.loadString('assets/dataset.json');
   final Map<String, dynamic> data = json.decode(response);
   return CategoryResponse.fromJson(data);
+}
+
+//items
+Future<ItemResponse> loadItems() async {
+  final String response = await rootBundle.loadString('assets/dataset.json');
+  final Map<String, dynamic> data = json.decode(response);
+  return ItemResponse.fromJson(data);
 }
