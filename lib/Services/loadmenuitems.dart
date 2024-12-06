7b4c839250e2c3dac30d9006d5881ed07a189dae
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-12-07 04:19:40 +0530

    new function added

--- lib/Services/loadmenuitems.dart
+++ lib/Services/loadmenuitems.dart
@@ -1,10 +1,17 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 
+import '../models/categorymodel.dart';
 import '../models/menu-item.dart';
 
 Future<MenuResponse> loadMenuItems() async {
   final String response = await rootBundle.loadString('assets/dataset.json');
   final Map<String, dynamic> data = json.decode(response);
   return MenuResponse.fromJson(data);
+}
+
+Future<CategoryResponse> loadCategories() async {
+  final String response = await rootBundle.loadString('assets/dataset.json');
+  final Map<String, dynamic> data = json.decode(response);
+  return CategoryResponse.fromJson(data);
 }
